71322af9727f281a0c2d2dccdcff2eff2a725ea4
Xavier <xrahman@students.cs.ubc.ca>
2023-12-29 08:49:55 -0800

    Added extra tests

--- src/test/MathParserTest.java
+++ src/test/MathParserTest.java
@@ -14,57 +14,74 @@ class MathParserTest {
     void testBinaryAddition() {
         assertEquals(7.0, parser.parseExpression("3+4"));
         assertEquals(9.0, parser.parseExpression("3+4+2"));
+        assertEquals(0.0, parser.parseExpression("–3+3"));
+        assertEquals(10.0, parser.parseExpression("5+2+3"));
     }
+
 
     @Test
     void testBinarySubtraction() {
         assertEquals(1.0, parser.parseExpression("5-4"));
         assertEquals(-1.0, parser.parseExpression("3-4"));
         assertEquals(-8.0, parser.parseExpression("3-4-7"));
+        assertEquals(0.0, parser.parseExpression("5-2-3"));
+        assertEquals(-10.0, parser.parseExpression("–5-5"));
     }
 
     @Test
     void testUnarySubtraction() {
-        // Simple unary subtraction
         assertEquals(-5, parser.parseExpression("–5"), 0.001);
-        // Unary subtraction with addition
-        assertEquals(-1, parser.parseExpression("–5 + 4"), 0.001);
-        // Unary subtraction with multiplication
+        assertEquals(-1, parser.parseExpression("–5+4"), 0.001);
         assertEquals(-20, parser.parseExpression("–5×4"), 0.001);
-        // Unary subtraction with parentheses
         assertEquals(-9, parser.parseExpression("–(4+5)"), 0.001);
-        // Multiple unary operators
         assertEquals(5, parser.parseExpression("–(–5)"), 0.001);
-        // Unary subtraction in complex expression
-        assertEquals(6.0, parser.parseExpression("3 × (–4 + 6)"), 0.001);
+        assertEquals(6.0, parser.parseExpression("3×(–4+6)"), 0.001);
         assertEquals(27.0, parser.parseExpression("–(–(5+4)×3)"), 0.001);
     }
 
     @Test
     void testMultiplication() {
         assertEquals(20.0, parser.parseExpression("5×4"));
+        assertEquals(0.0, parser.parseExpression("0×4"));
+        assertEquals(-15.0, parser.parseExpression("–3×5"));
+        assertEquals(12.0, parser.parseExpression("3×4×1"));
+        assertEquals(-36.0, parser.parseExpression("3×4×–3"));
     }
+
 
     @Test
     void testDivision() {
         assertEquals(2.0, parser.parseExpression("8÷4"));
         assertEquals(0.75, parser.parseExpression("3÷4"));
+        assertEquals(-2.0, parser.parseExpression("–8÷4"));
+        assertEquals(0.5, parser.parseExpression("1÷2"));
     }
+
 
     @Test
     void testDecimalHandling() {
         assertEquals(5.5, parser.parseExpression("2.5+3"));
+        assertEquals(7.3, parser.parseExpression("4.1+3.2"));
+        assertEquals(-2.56, parser.parseExpression("–1.28×2"));
+        assertEquals(3.333333333, parser.parseExpression("10÷3"), 0.000000001);
     }
+
 
     @Test
     void testParentheses() {
         assertEquals(7.0, parser.parseExpression("(3+4)×(2-1)"));
+        assertEquals(14.0, parser.parseExpression("(3+4)×(2)"));
+        assertEquals(21.0, parser.parseExpression("3×(7-1+1)"));
+        assertEquals(-4.0, parser.parseExpression("–(3+1)"));
     }
+
 
     @Test
     void testImplicitMultiplication() {
         assertEquals(6.0, parser.parseExpression("(3)(2)"));
         assertEquals(1.8, parser.parseExpression(".6(3)"), 0.001);
+        assertEquals(-6.0, parser.parseExpression("–(3)(2)"));
+        assertEquals(9.0, parser.parseExpression("(3)(3)"));
     }
 
     @Test
@@ -84,34 +101,30 @@ class MathParserTest {
 
     @Test
     void testMismatchedParentheses() {
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("(2 + 3"));
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2 + 3)"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("(2+3"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2+3)"));
     }
-
 
     @Test
     void testIncorrectOperatorUsage() {
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2 ++ 3"));
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2 + ÷ 3"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2++3"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2+÷3"));
     }
 
     @Test
     void testDivisionByZero() {
-        assertThrows(ArithmeticException.class, () -> parser.parseExpression("10 ÷ 0"));
+        assertThrows(ArithmeticException.class, () -> parser.parseExpression("10÷0"));
     }
 
     @Test
     void testInvalidSyntax() {
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("× 2 +3"));
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2 +"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("×2+3"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2+"));
     }
 
     @Test
     void testInvalidNumberFormats() {
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2..5 + 3"));
-        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2.3.4 + 5"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2..5+3"));
+        assertThrows(IllegalArgumentException.class, () -> parser.parseExpression("2.3.4+5"));
     }
-
-
-    // Add more tests for edge cases and complex expressions
 }
